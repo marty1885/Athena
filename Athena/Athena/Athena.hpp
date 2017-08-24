7893e49b670339aaebfeb3799e4bd053ba5079a0
marty1885 <marty188586@gmail.com>
2017-08-24 10:46:50 +0800

    implement basic general MLP

--- Athena/Athena/Athena.hpp
+++ Athena/Athena/Athena.hpp
@@ -4,8 +4,143 @@
 #include <xtensor/xrandom.hpp>
 #include <xtensor/xio.hpp>
 #include <xtensor/xindexview.hpp>
+#include <xtensor/xvectorize.hpp>
 #include <xtensor-blas/xlinalg.hpp>
+
+#include <assert.h>
+
+#include <vector>
+#include <iostream>
 
 namespace At
 {
+
+class Layer
+{
+public:
+	Layer(int input, int output):
+		mInputShape({input}), mOutputShape({output})
+	{
+	}
+
+	virtual void forward(const xt::xarray<float>& input, xt::xarray<float>& output)
+	{
+	}
+
+	virtual void backword(const xt::xarray<float>& x, xt::xarray<float>& y,
+			xt::xarray<float>& dx, const xt::xarray<float>& dy)
+	{
+	}
+
+// protected:
+	std::vector<xt::xarray<float>> mWeights;
+	std::vector<int> mInputShape;
+	std::vector<int> mOutputShape;
+};
+
+class FullyConnected : public Layer
+{
+public:
+	FullyConnected(int input, int output):
+		Layer(input, output)
+	{
+		mWeights.push_back(2 * xt::random::rand<float>({input, output}) - 1);
+		mWeights.push_back(2 * xt::random::rand<float>({output}) - 1);
+	}
+
+	virtual void forward(const xt::xarray<float>& x, xt::xarray<float>& y) override
+	{
+		y = xt::linalg::dot(x, mWeights[0])+mWeights[1];
+	}
+
+	virtual void backword(const xt::xarray<float>& x, xt::xarray<float>& y,
+			xt::xarray<float>& dx, const xt::xarray<float>& dy) override
+	{
+		dx = xt::linalg::dot(dy,xt::transpose(mWeights[0]));
+	}
+};
+
+class SigmoidLayer : public Layer
+{
+public:
+	virtual void forward(const xt::xarray<float>& x, xt::xarray<float>& y) override
+	{
+		y = 1/(1+xt::exp(x));
+	}
+
+	virtual void backword(const xt::xarray<float>& x, xt::xarray<float>& y,
+			xt::xarray<float>& dx, const xt::xarray<float>& dy) override
+	{
+		dx = dy * (y * (1 - y));
+	}
+};
+
+class SequentialNetwork
+{
+public:
+	inline xt::xarray<float> activate(const xt::xarray<float>& x, bool diriv = false)
+	{
+		if(diriv)
+			return x*(1-x);
+		return 1/(1+xt::exp(-x));
+	}
+
+	template<typename LayerType, typename ... Args>
+	void add(Args ... args)
+	{
+		mLayers.push_back(new LayerType(args ...));
+	}
+
+	void fit(const xt::xarray<float>& input, const xt::xarray<float>& desireOutput,
+		int epoch)
+	{
+		int datasetSize = input.shape()[0];
+		float L = 0.45f;
+		for(int i=0;i<epoch;i++)
+		{
+			for(int j=0;j<datasetSize;j++)
+			{
+				xt::xarray<float> x = xt::view(input,j,xt::all(),xt::all());
+				xt::xarray<float> y = xt::view(desireOutput,j,xt::all(),xt::all());
+
+				//TODO: handle N-D data
+				x.reshape({1,x.shape()[0]});
+				y.reshape({1,y.shape()[0]});
+
+				std::vector<xt::xarray<float>> layerOutputs;
+				layerOutputs.push_back(x);
+
+				for(auto& layer : mLayers)
+				{
+					auto& currentInput = layerOutputs.back();
+					xt::xarray<float> out;
+					layer->forward(currentInput, out);
+					out = activate(out);
+					layerOutputs.push_back(out);
+				}
+
+				xt::xarray<float> E = y - layerOutputs.back();
+				xt::xarray<float> dE = E * activate(layerOutputs.back(), true);
+
+				for(int k=mLayers.size()-1;k>=0;k--)
+				{
+					auto& layer = mLayers[k];
+					xt::xarray<float> tmp;
+					layer->backword(layerOutputs[k],layerOutputs[k+1], tmp, dE);
+					tmp *= activate(layerOutputs[k], true);
+					layer->mWeights[0] += xt::linalg::dot(xt::transpose(layerOutputs[k]), dE)*L;
+					layer->mWeights[1] += dE*L;
+
+					dE = tmp;
+				}
+
+				std::cout << E << std::endl;
+			}
+
+		}
+	}
+protected:
+	std::vector<Layer*> mLayers;
+};
+
 }
--- examples/example1/example1.cpp
+++ examples/example1/example1.cpp
@@ -1,6 +1,5 @@
 #include <Athena/Athena.hpp>
 
-
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -15,47 +14,15 @@ inline xt::xarray<float> activate(const xt::xarray<float>& x, bool diriv = false
 
 int main()
 {
-	unsigned int epoch = 3000;
-	int inputLayerSize = 2;
-	int hiddenLayerSize = 5;
-	int outputLayerSize = 1;
-	float learningRate = 0.45;
 
-	xt::xarray<float> Wh = 2.0f * xt::random::rand<float>({inputLayerSize,hiddenLayerSize}) - 1;
-	xt::xarray<float> Wz = 2.0f * xt::random::rand<float>({hiddenLayerSize,outputLayerSize}) - 1;
-	xt::xarray<float> Bh = 2.0f * xt::random::rand<float>({hiddenLayerSize}) - 1;
-	xt::xarray<float> Bz = 2.0f * xt::random::rand<float>({outputLayerSize}) - 1;
+	xt::xarray<float> X = {{0,0},{1,0},{0,1},{1,1}};
+	xt::xarray<float> Y = {{0,1,1,0}};
+	Y = xt::transpose(Y);
 
-	xt::xarray<float> X = {{1,0},{0,1},{1,1},{0,0}};
-	unsigned int datasetSize = X.shape()[0];
+	int epoch = 3000;
 
-	//Workarround xtensor issue #389 (https://github.com/QuantStack/xtensor/issues/389)
-	//TODO: Remove this workarround after bug in xtensor fixed
-	xt::xarray<float> tmp = {{1,1,0,0}};
-	auto Y = xt::transpose(tmp);
-
-	for(unsigned int i=0;i<epoch;i++)
-	{
-		float lossSum = 0;
-		for(unsigned int j=0;j<datasetSize;j++)
-		{
-			xt::xarray<float> Xb = xt::view(X,j,xt::all(),xt::all());
-			xt::xarray<float> Yb = xt::index_view(Y,{{0,j}});
-
-			xt::xarray<float> H = activate(xt::linalg::dot(Xb, Wh)+Bh);
-			xt::xarray<float> Z = activate(xt::linalg::dot(H, Wz)+Bz);
-			xt::xarray<float> E = Yb - Z;
-
-			xt::xarray<float> dZ = E * activate(Z, true);
-			xt::xarray<float> dH = xt::linalg::dot(dZ, xt::transpose(Wz)) * activate(H, true);
-			Wz += xt::linalg::dot(xt::transpose(H), dZ)*learningRate;
-			Wh += xt::linalg::dot(xt::transpose(Xb), dH)*learningRate;
-			Bz += dZ*learningRate;
-			Bh += dH*learningRate;
-
-			lossSum += ((xt::xarray<float>)xt::sum(xt::pow(E,2)))[0];
-		}
-
-		cout << lossSum/datasetSize << endl;
-	}
+	At::SequentialNetwork net;
+	net.add<At::FullyConnected>(2,5);
+	net.add<At::FullyConnected>(5,1);
+	net.fit(X,Y,epoch);
 }
