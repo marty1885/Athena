5b169ea8a52a102d29cb0ebb19ae383e51aaf359
marty1885 <marty188586@gmail.com>
2018-05-21 09:49:49 +0800

    Tensor::loadStates respects current backend

--- Athena/Tensor.cpp
+++ Athena/Tensor.cpp
@@ -8,26 +8,27 @@ void Tensor::loadStates(const BoxedValues& states)
 {
 	const auto& s = states.get<Shape>("shape");
 	auto boxedPtr = states.ptr("values");
+	Backend* bk = backend()==nullptr ? defaultBackend() : backend();
 
 	if(auto ptr = boxed_cast<std::vector<float>>(boxedPtr); boxedPtr != nullptr)
 	{
 		const auto& values = states.get<std::vector<float>>("values");
-		*this = Tensor(values, s);
+		*this = Tensor(values, s, *bk);
 	}
 	else if(auto ptr = boxed_cast<std::vector<double>>(boxedPtr); boxedPtr != nullptr)
 	{
 		const auto& values = states.get<std::vector<double>>("values");
-		*this = Tensor(values, s);
+		*this = Tensor(values, s, *bk);
 	}
 	else if(auto ptr = boxed_cast<std::vector<int32_t>>(boxedPtr); boxedPtr != nullptr)
 	{
 		const auto& values = states.get<std::vector<int32_t>>("values");
-		*this = Tensor(values, s);
+		*this = Tensor(values, s, *bk);
 	}
 	else if(auto ptr = boxed_cast<std::vector<int32_t>>(boxedPtr); boxedPtr != nullptr)
 	{
 		const auto& values = states.get<std::vector<int32_t>>("values");
-		*this = Tensor(values, s);
+		*this = Tensor(values, s, *bk);
 	}
 }
 
